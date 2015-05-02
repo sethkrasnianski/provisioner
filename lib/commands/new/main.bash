a4333e440bcee7a342c2f1e39116c50d8e0c86f2
Seth Krasnianski <seth@mojotech.com>
2015-05-02 16:21:42 -0400

    lib/commands: Setup help for new command

--- lib/commands/new/main.bash
+++ lib/commands/new/main.bash
@@ -1,3 +1,19 @@
-new() {
-  echo "new VM";
-}
+source $lib_dir/help/new.bash
+
+if [ -n "${DO_TOKEN+1}" ]; then
+  echo "DO_TOKEN required to make requests."
+else
+  new() {
+    local args=$@
+
+    for arg in $args
+    do
+      case $arg in
+        -h | --help ) help_new
+                      shift
+                      ;;
+      esac
+      shift
+    done
+  }
+fi
